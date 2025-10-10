51339f55afcb819515f4750718f0fa55bc477ea4
Wadswow <nicholas102spy@gmail.com>
2025-10-10 02:13:45 +0000

    initial set-up commit
    Co-authored-by: kaushikrajarathinam <kaushikrajarathinam@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,5 @@ import "./style.css";
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 `;
+
+// I reviewed this code, for now both of us have this incomplete.
