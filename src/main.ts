bb66b03371017b1f0334f0d1da1097217eccc99d
Wadswow <nicholas102spy@gmail.com>
2025-10-15 21:51:22 +0000

    changed title of webpage and added button that increments upon mouse click (Step 2 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import sword from "./sword.jpg";
 import "./style.css";
 
+let counter: number = 0;
+
 document.body.innerHTML = `
   <h1>Welcome to Sword Sharpener</h1>
   <p><button id="sharpened"><img src="${sword}" class="icon" /></button></p>
   <p>^ click the sword to sharpen</p>
   <h4>Swords Sharpened: <span id ="counter">0</span></h4>
 `;
+
+const button = document.getElementById("sharpened")!;
+const counterElement = document.getElementById("counter")!;
+
+button.addEventListener("click", () => {
+  counter += 1;
+  counterElement.innerHTML = counter.toString();
+});
