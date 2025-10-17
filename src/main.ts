574fbedc7fbfa5acc9166e0e437afe69d303542e
Wadswow <nicholas102spy@gmail.com>
2025-10-17 21:53:27 +0000

    Made a button that can purchase an upgrade that activates the autoclicker and also deactivates when counter is less than 10 (step 5 complete)

--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,11 @@ import sword from "./sword.jpg";
 import "./style.css";
 
 let counter: number = 0;
-
-document.body.innerHTML = `
-  <h1>Welcome to Sword Sharpener</h1>
-  <p><button id="sharpened"><img src="${sword}" class="icon" /></button></p>
-  <p>^ click the sword to sharpen</p>
-  <h4>Swords Sharpened: <span id ="counter">0</span></h4>
-`;
-
-const button = document.getElementById("sharpened")!;
-const counterElement = document.getElementById("counter")!;
-
-button.addEventListener("click", () => {
-  counter += 1;
-  counterElement.innerHTML = counter.toString();
-});
-
+let totalGrind: number = 0;
 const zero = performance.now();
 let lastSecond = -1;
+
+//autoclicker handler (using requstAnimationFrame)
 function autoclick(currentTime: number) {
   const elapsed = Math.floor((currentTime - zero) / 1000);
   if (elapsed < 1000) {
@@ -27,8 +14,47 @@ function autoclick(currentTime: number) {
       counter += 1;
       counterElement.innerHTML = counter.toString();
       lastSecond = elapsed;
+      if (counter >= 10) {
+        upgradeButton.disabled = false;
+      }
     }
     requestAnimationFrame(autoclick);
   }
 }
-requestAnimationFrame(autoclick);
+
+document.body.innerHTML = `
+  <h1>Welcome to Sword Sharpener</h1>
+  <p><button id="sharpened"><img src="${sword}" class="icon" /></button></p>
+  <p>^ click the sword to sharpen</p>
+  <h4>Swords Sharpened: <span id ="counter">0</span></h4>
+  <p>Buy Grindstone: <span id ="totalGrind">0</span> <button id="grindstone" disabled>Buy</button></p>
+`;
+
+const clickButton = document.getElementById("sharpened")!;
+const upgradeButton = document.getElementById(
+  "grindstone",
+) as HTMLButtonElement;
+const counterElement = document.getElementById("counter")!;
+const grindElement = document.getElementById("totalGrind")!;
+
+//button click handler
+clickButton.addEventListener("click", () => {
+  counter += 1;
+  counterElement.innerHTML = counter.toString();
+  if (counter >= 10) {
+    upgradeButton.disabled = false;
+  }
+});
+
+upgradeButton.addEventListener("click", () => {
+  totalGrind += 1;
+  counter -= 10;
+  counterElement.innerHTML = counter.toString();
+  grindElement.innerHTML = totalGrind.toString();
+  if (totalGrind > 0) {
+    requestAnimationFrame(autoclick);
+  }
+  if (counter < 10) {
+    upgradeButton.disabled = true;
+  }
+});
